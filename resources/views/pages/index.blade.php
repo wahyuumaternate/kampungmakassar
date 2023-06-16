715cf400986962d4c792ddbbdceed22571c7738d
wahyuumaternate <retmujago@gmail.com>
2023-06-16 14:09:27 +0900

    wahyu

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -144,8 +144,8 @@
 
                             <div class="entry-meta">
                                 <ul>
-                                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a
-                                            href="blog-single.html">John Doe</a></li>
+                                    <li class="d-flex align-items-center"><i class="bi bi-eye"></i> <a
+                                            href="blog-single.html">200</a></li>
                                     <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
                                             href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a>
                                     </li>
@@ -177,8 +177,8 @@
 
                             <div class="entry-meta">
                                 <ul>
-                                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a
-                                            href="blog-single.html">John Doe</a></li>
+                                    <li class="d-flex align-items-center"><i class="bi bi-eye"></i> <a
+                                            href="blog-single.html">200</a></li>
                                     <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
                                             href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a>
                                     </li>
@@ -213,8 +213,8 @@
 
                             <div class="entry-meta">
                                 <ul>
-                                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a
-                                            href="blog-single.html">John Doe</a></li>
+                                    <li class="d-flex align-items-center"><i class="bi bi-eye"></i> <a
+                                            href="blog-single.html">200</a></li>
                                     <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
                                             href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a>
                                     </li>
@@ -248,8 +248,8 @@
 
                             <div class="entry-meta">
                                 <ul>
-                                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a
-                                            href="blog-single.html">John Doe</a></li>
+                                    <li class="d-flex align-items-center"><i class="bi bi-eye"></i> <a
+                                            href="blog-single.html">200</a></li>
                                     <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
                                             href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a>
                                     </li>
