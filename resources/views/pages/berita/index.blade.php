adb8b36f2c299b81491d4b0bb097b78c4f9bbe66
aryaddinata <aryaddinataabbas017@gmail.com>
2023-06-17 00:32:26 +0900

    Keempat

--- resources/views/pages/berita/index.blade.php
+++ resources/views/pages/berita/index.blade.php
@@ -6,18 +6,230 @@
     @include('layouts.hero')
 @endsection
 
-<!-- ======= BERITA Section ======= -->
-<section id="struktur_organisasi" class="blog">
+<!-- ======= posts Section ======= -->
+<section id="blog" class="blog">
     <div class="container" data-aos="fade-up">
         <div class="section-title">
-            <h2>BERITA</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eleifend neque a lectus consequat, vitae pharetra lectus consectetur. Nam efficitur dapibus est, ut venenatis eros tincidunt at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras gravida purus ac ligula aliquet, id consequat velit sagittis. Nunc vitae risus aliquet, posuere elit id, sagittis nulla. Curabitur quis dui pretium, convallis nisi vitae, lobortis dolor. Fusce lacinia vulputate arcu, at feugiat nulla bibendum ut. Integer in leo at ipsum pharetra aliquet. Suspendisse semper orci quis orci interdum, non dictum sapien fermentum. Curabitur consequat elit a bibendum fringilla.<br>
+            <h2>Berita Terkini</h2>
+        </div>
+        <div class="row">
 
-Sed rhoncus, ipsum nec varius efficitur, sem arcu tristique arcu, vitae placerat massa ante in nisi. Etiam consequat, lectus et semper iaculis, metus purus facilisis massa, a pulvinar neque est nec urna. Fusce consequat dolor lacus, nec lobortis lacus finibus id. Integer et consectetur metus. Aliquam at nunc nec mi consectetur consequat sed sit amet dolor. Mauris finibus placerat dolor, in scelerisque risus faucibus in. Nulla dapibus bibendum aliquam. Suspendisse consectetur eleifend ligula, in viverra lacus pretium non. Curabitur euismod metus vel diam ultricies, nec feugiat metus scelerisque. Sed vel justo ut risus euismod suscipit. Donec at rhoncus nunc.<br>
+            <div class="col-lg-4">
 
-Pellentesque bibendum malesuada leo, nec euismod massa congue ac. Sed pulvinar, risus ac cursus dapibus, nibh elit tempor turpis, eu dapibus velit ante id nisi. Phasellus maximus ipsum lacus, ut dignissim enim consequat non. Nunc at mi bibendum, congue mauris non, efficitur ex. Nullam ut lobortis leo, vitae malesuada nibh. Vivamus posuere velit sit amet turpis sollicitudin, et consectetur nulla condimentum. Ut non dolor nulla. Aliquam posuere arcu id risus interdum, a mattis odio volutpat. Donec at ex ligula. Fusce efficitur consectetur ligula, sit amet rhoncus massa varius id. Morbi vel sollicitudin ligula. Nunc eu velit consectetur, fermentum nibh eget, elementum justo. Nunc interdum mi eu odio maximus, id fermentum turpis condimentum.</p>
+                <div class="sidebar sticky-top">
+
+                    <h3 class="sidebar-title">Search</h3>
+                    <div class="sidebar-item search-form">
+                        <form action="">
+                            <input type="text">
+                            <button type="submit"><i class="bi bi-search"></i></button>
+                        </form>
+                    </div><!-- End sidebar search formn-->
+
+
+
+                    <h3 class="sidebar-title">Sambutan Lurah</h3>
+                    <div class="sidebar-item recent-posts">
+                        <div class="post-item clearfix">
+                            <img src="assets/img/blog/blog-recent-1.jpg" alt="">
+                            <h4><a href="blog-single.html">Nihil blanditiis at in nihil autem</a></h4>
+                            <time datetime="2020-01-01">Jan 1, 2020</time>
+                        </div>
+                    </div><!-- End sidebar recent posts-->
+
+                    <h3 class="sidebar-title">Recent Posts</h3>
+                    <div class="sidebar-item recent-posts">
+                        <div class="post-item clearfix">
+                            <img src="assets/img/blog/blog-recent-1.jpg" alt="">
+                            <h4><a href="blog-single.html">Nihil blanditiis at in nihil autem</a></h4>
+                            <time datetime="2020-01-01">Jan 1, 2020</time>
+                        </div>
+
+                        <div class="post-item clearfix">
+                            <img src="assets/img/blog/blog-recent-2.jpg" alt="">
+                            <h4><a href="blog-single.html">Quidem autem et impedit</a></h4>
+                            <time datetime="2020-01-01">Jan 1, 2020</time>
+                        </div>
+
+                        <div class="post-item clearfix">
+                            <img src="assets/img/blog/blog-recent-3.jpg" alt="">
+                            <h4><a href="blog-single.html">Id quia et et ut maxime similique occaecati ut</a></h4>
+                            <time datetime="2020-01-01">Jan 1, 2020</time>
+                        </div>
+
+                        <div class="post-item clearfix">
+                            <img src="assets/img/blog/blog-recent-4.jpg" alt="">
+                            <h4><a href="blog-single.html">Laborum corporis quo dara net para</a></h4>
+                            <time datetime="2020-01-01">Jan 1, 2020</time>
+                        </div>
+
+                        <div class="post-item clearfix">
+                            <img src="assets/img/blog/blog-recent-5.jpg" alt="">
+                            <h4><a href="blog-single.html">Et dolores corrupti quae illo quod dolor</a></h4>
+                            <time datetime="2020-01-01">Jan 1, 2020</time>
+                        </div>
+
+                    </div><!-- End sidebar recent posts-->
+                </div><!-- End sidebar -->
+
+            </div><!-- End blog sidebar -->
+
+            {{-- <div class="col-lg-1"></div> --}}
+
+            <div class="col-lg-8 entries">
+                <div class="row">
+                    <div class="col-lg-6 col-sm-12">
+                        <article class="entry">
+
+                            <div class="entry-img">
+                                <img src="assets/img/blog/blog-1.jpg" alt="" class="img-fluid">
+                            </div>
+
+                            <h2 class="entry-title">
+                                <a href="blog-single.html">Dolorum optio tempore voluptas dignissimos cumque fuga qui
+                                    quibusdam quia</a>
+                            </h2>
+
+                            <div class="entry-meta">
+                                <ul>
+                                    <li class="d-flex align-items-center"><i class="bi bi-eye"></i> <a
+                                            href="blog-single.html">200</a></li>
+                                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
+                                            href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="entry-content">
+                                <p>
+                                    Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi
+                                    praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
+                                </p>
+                                <div class="read-more">
+                                    <a href="blog-single.html">Read More</a>
+                                </div>
+                            </div>
+
+                        </article><!-- End blog entry -->
+                    </div>
+                    <div class="col-lg-6 col-sm-12">
+                        <article class="entry">
+
+                            <div class="entry-img">
+                                <img src="assets/img/blog/blog-2.jpg" alt="" class="img-fluid">
+                            </div>
+
+                            <h2 class="entry-title">
+                                <a href="blog-single.html">Nisi magni odit consequatur autem nulla dolorem</a>
+                            </h2>
+
+                            <div class="entry-meta">
+                                <ul>
+                                    <li class="d-flex align-items-center"><i class="bi bi-eye"></i> <a
+                                            href="blog-single.html">200</a></li>
+                                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
+                                            href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="entry-content">
+                                <p>
+                                    Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi
+                                    praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
+                                </p>
+                                <div class="read-more">
+                                    <a href="blog-single.html">Read More</a>
+                                </div>
+                            </div>
+
+                        </article><!-- End blog entry -->
+                    </div>
+                    <div class="col-lg-6 col-sm-12">
+
+                        <article class="entry">
+
+                            <div class="entry-img">
+                                <img src="assets/img/blog/blog-3.jpg" alt="" class="img-fluid">
+                            </div>
+
+                            <h2 class="entry-title">
+                                <a href="blog-single.html">Possimus soluta ut id suscipit ea ut. In quo quia et soluta
+                                    libero
+                                    sit sint.</a>
+                            </h2>
+
+                            <div class="entry-meta">
+                                <ul>
+                                    <li class="d-flex align-items-center"><i class="bi bi-eye"></i> <a
+                                            href="blog-single.html">200</a></li>
+                                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
+                                            href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="entry-content">
+                                <p>
+                                    Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi
+                                    praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
+                                </p>
+                                <div class="read-more">
+                                    <a href="blog-single.html">Read More</a>
+                                </div>
+                            </div>
+
+                        </article><!-- End blog entry -->
+                    </div>
+                    <div class="col-lg-6 col-sm-12">
+                        <article class="entry">
+
+                            <div class="entry-img">
+                                <img src="assets/img/blog/blog-4.jpg" alt="" class="img-fluid">
+                            </div>
+
+                            <h2 class="entry-title">
+                                <a href="blog-single.html">Non rem rerum nam cum quo minus. Dolor distinctio deleniti
+                                    explicabo
+                                    eius exercitationem.</a>
+                            </h2>
+
+                            <div class="entry-meta">
+                                <ul>
+                                    <li class="d-flex align-items-center"><i class="bi bi-eye"></i> <a
+                                            href="blog-single.html">200</a></li>
+                                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a
+                                            href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="entry-content">
+                                <p>
+                                    Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi
+                                    praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
+                                </p>
+                                <div class="read-more">
+                                    <a href="blog-single.html">Read More</a>
+                                </div>
+                            </div>
+
+                        </article><!-- End blog entry -->
+                    </div>
+                </div>
+                <div class="blog-pagination">
+                    <ul class="justify-content-center">
+                        <li><a href="#">1</a></li>
+                        <li class="active"><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                    </ul>
+                </div>
+
+            </div><!-- End blog entries list -->
+
+        </div>
+
     </div>
-</section><!-- End BERITA Section -->
-
-@include('layouts.footer')
-@endsection
+</section><!-- End posts Section -->
+            @include('layouts.footer')
+        @endsection
