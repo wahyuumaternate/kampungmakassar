7512d6a6cf728be111b68d89b8c824617429e27e
aryaddinata <aryaddinataabbas017@gmail.com>
2023-06-19 19:55:12 +0900

    Keenam

--- resources/views/pages/pengaduan/index.blade.php
+++ resources/views/pages/pengaduan/index.blade.php
@@ -19,6 +19,10 @@
                     </div>
                     <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label"></label>
+                        <input type="NIK" class="form-control" id="exampleFormControlInput1" placeholder="NIK">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label"></label>
                         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
                     </div>
                     <div class="mb-3">
@@ -29,6 +33,10 @@
                     <div class="mb-3">
                         <label for="exampleFormControlTextarea1" class="form-label">Isi Pengaduan</label>
                         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="formFileMultiple" class="form-label"></label>
+                        <input class="form-control" type="file" id="formFileMultiple" multiple>
                     </div>
                     <div class="col-auto">
                         <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
