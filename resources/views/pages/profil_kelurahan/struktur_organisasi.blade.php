0aa92d0e0748d43da400e398b0fc1b21c99b7bf4
aryaddinata <aryaddinataabbas017@gmail.com>
2023-06-20 01:20:02 +0900

    New Pull

--- resources/views/pages/profil_kelurahan/struktur_organisasi.blade.php
+++ resources/views/pages/profil_kelurahan/struktur_organisasi.blade.php
@@ -3,7 +3,7 @@
 @section('body')
 @section('outmain')
     @include('layouts.header')
-    @include('layouts.hero')
+    {{-- @include('layouts.hero') --}}
 @endsection
 
 <!-- ======= Struktur Organisasi Section ======= -->
@@ -11,12 +11,77 @@
     <div class="container" data-aos="fade-up">
         <div class="section-title">
             <h2>Struktur Organisasi</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eleifend neque a lectus consequat, vitae pharetra lectus consectetur. Nam efficitur dapibus est, ut venenatis eros tincidunt at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras gravida purus ac ligula aliquet, id consequat velit sagittis. Nunc vitae risus aliquet, posuere elit id, sagittis nulla. Curabitur quis dui pretium, convallis nisi vitae, lobortis dolor. Fusce lacinia vulputate arcu, at feugiat nulla bibendum ut. Integer in leo at ipsum pharetra aliquet. Suspendisse semper orci quis orci interdum, non dictum sapien fermentum. Curabitur consequat elit a bibendum fringilla.<br>
-
-Sed rhoncus, ipsum nec varius efficitur, sem arcu tristique arcu, vitae placerat massa ante in nisi. Etiam consequat, lectus et semper iaculis, metus purus facilisis massa, a pulvinar neque est nec urna. Fusce consequat dolor lacus, nec lobortis lacus finibus id. Integer et consectetur metus. Aliquam at nunc nec mi consectetur consequat sed sit amet dolor. Mauris finibus placerat dolor, in scelerisque risus faucibus in. Nulla dapibus bibendum aliquam. Suspendisse consectetur eleifend ligula, in viverra lacus pretium non. Curabitur euismod metus vel diam ultricies, nec feugiat metus scelerisque. Sed vel justo ut risus euismod suscipit. Donec at rhoncus nunc.<br>
-
-Pellentesque bibendum malesuada leo, nec euismod massa congue ac. Sed pulvinar, risus ac cursus dapibus, nibh elit tempor turpis, eu dapibus velit ante id nisi. Phasellus maximus ipsum lacus, ut dignissim enim consequat non. Nunc at mi bibendum, congue mauris non, efficitur ex. Nullam ut lobortis leo, vitae malesuada nibh. Vivamus posuere velit sit amet turpis sollicitudin, et consectetur nulla condimentum. Ut non dolor nulla. Aliquam posuere arcu id risus interdum, a mattis odio volutpat. Donec at ex ligula. Fusce efficitur consectetur ligula, sit amet rhoncus massa varius id. Morbi vel sollicitudin ligula. Nunc eu velit consectetur, fermentum nibh eget, elementum justo. Nunc interdum mi eu odio maximus, id fermentum turpis condimentum.</p>
-    </div>
+                <div class="row">
+                    <div class="col-12 text-center mb-5">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-6">
+                                <img src="assets/img/person-7.jpg" alt=""
+                                    class="img-fluid rounded-circle w-50 mb-4">
+                                <h4>Jane Smith</h4>
+                                <span class="d-block mb-3 text-uppercase">CEO</span>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil sint sed, fugit
+                                    distinctio ad eius itaque deserunt doloribus harum excepturi laudantium sit officiis
+                                    et eaque blanditiis. Dolore natus excepturi recusandae.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 text-center mb-5">
+                        <img src="assets/img/person-1.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+                        <h4>Cameron Williamson</h4>
+                        <span class="d-block mb-3 text-uppercase">Founder</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat
+                            maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque
+                            provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis
+                            sunt ullam temporibus aut!</p>
+                    </div>
+                    <div class="col-lg-4 text-center mb-5">
+                        <img src="assets/img/person-2.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+                        <h4>Wade Warren</h4>
+                        <span class="d-block mb-3 text-uppercase">Founder, VP</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat
+                            maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque
+                            provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis
+                            sunt ullam temporibus aut!</p>
+                    </div>
+                    <div class="col-lg-4 text-center mb-5">
+                        <img src="assets/img/person-3.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+                        <h4>Jane Cooper</h4>
+                        <span class="d-block mb-3 text-uppercase">Editor Staff</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat
+                            maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque
+                            provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis
+                            sunt ullam temporibus aut!</p>
+                    </div>
+                    <div class="col-lg-4 text-center mb-5">
+                        <img src="assets/img/person-4.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+                        <h4>Cameron Williamson</h4>
+                        <span class="d-block mb-3 text-uppercase">Editor Staff</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat
+                            maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque
+                            provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis
+                            sunt ullam temporibus aut!</p>
+                    </div>
+                    <div class="col-lg-4 text-center mb-5">
+                        <img src="assets/img/person-5.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+                        <h4>Cameron Williamson</h4>
+                        <span class="d-block mb-3 text-uppercase">Editor Staff</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat
+                            maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque
+                            provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis
+                            sunt ullam temporibus aut!</p>
+                    </div>
+                    <div class="col-lg-4 text-center mb-5">
+                        <img src="assets/img/person-6.jpg" alt="" class="img-fluid rounded-circle w-50 mb-4">
+                        <h4>Cameron Williamson</h4>
+                        <span class="d-block mb-3 text-uppercase">Editor Staff</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, perspiciatis repellat
+                            maxime, adipisci non ipsam at itaque rerum vitae, necessitatibus nulla animi expedita cumque
+                            provident inventore? Voluptatum in tempora earum deleniti, culpa odit veniam, ea reiciendis
+                            sunt ullam temporibus aut!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 </section><!-- End Struktur Organisasi Section -->
 
 @include('layouts.footer')
