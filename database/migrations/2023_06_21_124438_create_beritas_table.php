0451e20fccf4ec878e8f403d380c787be26a818c
wahyuumaternate <retmujago@gmail.com>
2023-06-22 01:27:01 +0900

    berita

--- database/migrations/2023_06_21_124438_create_beritas_table.php
+++ database/migrations/2023_06_21_124438_create_beritas_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('judul');
             $table->string('gambar');
-            $table->string('isi');
+            $table->text('isi');
             $table->string('slug');
             $table->string('excerp');
             $table->softDeletes();
