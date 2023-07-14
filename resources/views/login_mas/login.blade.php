f34d465a5782adf77597e8843966f66225f96d94
wahyuumaternate <retmujago@gmail.com>
2023-07-14 16:44:38 +0900

    100%

--- resources/views/login_mas/login.blade.php
+++ resources/views/login_mas/login.blade.php
@@ -21,7 +21,7 @@
                 </form>
                 <div class="d-flex justify-content-center">
                     @if (Route::has('password.request'))
-                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                        <a class="underline text-sm text-center"
                             href="{{ route('password.request') }}">
                             Lupa Kata Sandi?
                         </a>
